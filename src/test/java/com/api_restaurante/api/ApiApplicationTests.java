c00abd32f30f7bf68f22383a1c47b77b592f2ceb
FelipeOropeza <felipe2006.co@gmail.com>
2026-04-20 02:02:44 -0300

    chore: configurar variaveis de ambiente e protecao de segredos

---
+++ src/test/java/com/api_restaurante/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_restaurante.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
